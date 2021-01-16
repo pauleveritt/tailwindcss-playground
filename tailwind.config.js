df272778b711ee00e19d0c69b0b8fb7ae6ffbdc2
Paul Everitt <pauleveritt@me.com>
2021-01-16 14:33:15 -0500

    Clean up for demo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spacer: '#5c6ac6'
+      }
+    },
   },
   variants: {},
   plugins: [],
